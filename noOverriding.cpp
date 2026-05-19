0dafd2af57c62b6ff51b5338cf2e5aa40575d1a6
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-19 17:42:32 +0700

    membuat class dan fungsi utama

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -1,1 +1,22 @@
 #include <iostream> //library
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() final {
+         cout << "Hallo saya function dari base class";
+    }
+};
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan1();
+
+    return 0;
+}
