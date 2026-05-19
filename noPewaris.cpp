bebe2a06f2c99606946436d7460bdcc934f143e7
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-19 17:40:23 +0700

    membuat class dan fungsi no pewaris

--- noPewaris.cpp
+++ noPewaris.cpp
@@ -1,1 +1,31 @@
 #include <iostream> //library
+using namespace std;
+
+class baseClass final{
+public:
+    virtual void perkenalan (){
+        cout << "Hallo saya function dari base class";
+    }
+};
+
+class baseClass1 {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya function dari base class";
+    }
+};
+
+class derivedClass : public baseClass1 {
+public:
+    void perkenalan(){
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
+int main(){
+    derivedClass a;
+    a.perkenalan();
+    
+    return 0;
+    
+}
